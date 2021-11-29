d73ccc1c0c2108066c99c0a8d71bf52867c4b992
AlexandrDavidovscky850503 <61647805+AlexandrDavidovscky850503@users.noreply.github.com>
2021-11-29 15:58:47 +0300

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -9,7 +9,7 @@ import time
 STORAGE_DIR = 'storage'
 IP = ''
 
-PORT = 19012
+PORT = 19001
 BUFFER_SIZE = 1024
 
 TIMEOUT = 20
@@ -62,10 +62,9 @@ def handle_client_request(client, request):
         else:
             send_status_and_message(client['socket'], command, SERVER_ERROR, "No such file")
 
-    if command == "upload":
+    elif command == "upload":
         send_status(client['socket'], command, OK_STATUS)
         upload(client, params)
-
 
     elif command == "echo":
         send_status(client['socket'], command, OK_STATUS)
@@ -74,6 +73,7 @@ def handle_client_request(client, request):
     elif command == "time":
         send_status(client['socket'], command, OK_STATUS)
         send_time(client)
+
 
     elif command == "exit":
         send_status(client['socket'], command, OK_STATUS)
@@ -132,53 +132,8 @@ def get_data(client):
 def send_data(client, data):
     client['socket'].send(str(data).encode('utf-8'))
 
-def upload(client, file_name):
-    size = int(get_data(client))
-    print("size = ", size)
-    send_ok(client)
-
-    data_size_recv = get_data(client)
-    if (data_size_recv):
-        data_size_recv = int(data_size_recv)
-
-    waiting_client = search_by_ip(waiting_clients, client['ip'])
-    if (len(waiting_clients) > 0 and waiting_client != False):
-        waiting_clients.remove(waiting_client)
-
-    if (waiting_client):
-        if (waiting_client['file_name'] == file_name and waiting_client['command'] == 'upload'):
-            data_size_recv = int(waiting_client['progress'])
-            #print("data_size_recv (progress) = ",data_size_recv)
-            send_data(client, data_size_recv)
-    else:
-        send_data(client, data_size_recv)
-
-    send_ok(client)
-    if (data_size_recv == 0):
-        f = open(file_name, "wb")
-    else:
-        f = open(file_name, "rb+")
-    f.seek(data_size_recv, 0)
-    print("Start uploading")
-    while (data_size_recv < size):
-        try:
-            data = client['socket'].recv(BUFFER_SIZE)
-            f.write(data)
-            data_size_recv += len(data)
-            # print("data_size_recv",data_size_recv)
-            client['socket'].settimeout(0.0001)
-            f.seek(data_size_recv, 0)
-
-        except socket.error as e:
-            f.close()
-            handle_disconnect(client, "upload", file_name, data_size_recv)
-            client['is_closed'] = True
-            return
-    print("Upload finished")
-    f.close()
-
 def download(client, file_name):
-    obj_download = storage_path +'/'+file_name
+    obj_download = "C:/Users/xiaom/PycharmProjects/serv2/storage/"+file_name
     obj_download.encode('unicode_escape')
     f = open(obj_download, "rb+")
     size = int(os.path.getsize(obj_download))
@@ -249,6 +204,75 @@ def continue_download(client, received_data):
         client['is_downloading'] = False
         f.close()
 
+def continue_upload(found_client, request):
+    data_size_recv = int(received_data)
+    f = client['file']
+    size = int(os.path.getsize(f.name))
+    if (data_size_recv < size):
+        f.seek(data_size_recv)
+        try:
+            data_file = f.read(BUFFER_SIZE)
+            client['socket'].sendall(data_file)
+        except socket.error as e:
+            f.close()
+            handle_disconnect(client, "download", f.name, data_size_recv)
+            client['is_closed'] = True
+            return
+
+        except KeyboardInterrupt:
+            server.close()
+            client.socket.close()
+            os._exit(1)
+    else:
+        print(f.name, "was downloaded")
+        client['is_downloading'] = False
+        f.close()
+def upload(client, file_name):
+    size = int(get_data(client))
+    print("size = ", size)
+    send_ok(client)
+
+    data_size_recv = get_data(client)
+    if (data_size_recv):
+        data_size_recv = int(data_size_recv)
+
+    waiting_client = search_by_ip(waiting_clients, client['ip'])
+    if (len(waiting_clients) > 0 and waiting_client != False):
+        waiting_clients.remove(waiting_client)
+
+    if (waiting_client):
+        if (waiting_client['file_name'] == file_name and waiting_client['command'] == 'upload'):
+            data_size_recv = int(waiting_client['progress'])
+            #print("data_size_recv (progress) = ",data_size_recv)
+            send_data(client, data_size_recv)
+    else:
+        send_data(client, data_size_recv)
+
+    send_ok(client)
+    if (data_size_recv == 0):
+        f = open(file_name, "wb")
+    else:
+        f = open(file_name, "rb+")
+    f.seek(data_size_recv, 0)
+    print("Start uploading")
+    # client['is_uploading'] = True
+    # client['file'] = f
+    while (data_size_recv < size):
+        try:
+            data = client['socket'].recv(BUFFER_SIZE)
+            f.write(data)
+            data_size_recv += len(data)
+            # print("data_size_recv",data_size_recv)
+            client['socket'].settimeout(0.0001)
+            f.seek(data_size_recv, 0)
+            #return
+        except socket.error as e:
+            f.close()
+            handle_disconnect(client, "upload", file_name, data_size_recv)
+            client['is_closed'] = True
+            return
+    print("Upload finished")
+    f.close()
 
 cur_dir = os.path.abspath(os.path.curdir)  # Получить абсолютный путь файла или каталога
 storage_path = os.path.join(cur_dir,STORAGE_DIR)  # правильно соединяет переданный путь cur_dir к одному или более компонентов пути *STORAGE_DIR
@@ -292,6 +316,7 @@ while True:
                             "is_closed": False,
                             "port": client_port,
                             "is_downloading": False,
+                            "is_uploading": False,
                             "file": ''
                         }
 
@@ -306,6 +331,9 @@ while True:
                 found_client = search_by_socket(clients_pool, ready_socket)
                 if found_client['is_downloading']:
                     continue_download(found_client, request)
+                elif found_client['is_uploading']:
+                    continue_upload(found_client, request)
+
                 elif request:
                     request = request.strip()
                     if request != '':
