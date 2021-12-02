9a5a72803bc8f010a0dae552594afc727c6e3b40
Anton Kreis <61134563+antonkreis850503@users.noreply.github.com>
2021-12-02 15:13:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import threading
 import time
 import socket
 import datetime
+import random
 
 DOWNLOAD_SERVICE_PORT = 50001
 UPLOAD_SERVICE_PORT = 50002
 ECHO_SERVICE_PORT = 50003
 TIME_SERVICE_PORT = 50004
 
-server_download_sock = None
-server_upload_sock = None
-server_echo_sock = None
-server_time_sock = None
+# server_download_sock = None
+# server_upload_sock = None
+# server_echo_sock = None
+# server_time_sock = None
+
+LOCK_ECHO = threading.Lock()
+
+users_echo_info = []
 
 UDP_BUFFER_SIZE = 32768
 UDP_DATAGRAMS_AMOUNT = 15
@@ -24,12 +29,12 @@ def create_sock(port_num):
     return new_socket
 
 
-def udp_recv_1(sock, bytes_amount, timeout, datagrams_amount, wait_flag = False):
+def udp_recv_1(sock, user, bytes_amount, timeout, datagrams_amount, wait_flag = False):
     # print('a')
-    global clients_addr
-    global datagram_count_in
-    global datagram_count_out
-    # datagram_count_in_old = datagram_count_in
+    addr = user['address']
+    datagram_count_in = user['datagram_count_in']
+    datagram_count_out = user['datagram_count_out']
+
     datagram_count_in_begin = datagram_count_in
 
     exc_flag = False
@@ -40,7 +45,6 @@ def udp_recv_1(sock, bytes_amount, timeout, datagrams_amount, wait_flag = False)
     req = 0
 
     i_temp = 0
-    addr = ('127.0.0.1', 0)
 
     while 1:
         i = i_temp
@@ -122,18 +126,65 @@ def udp_recv_1(sock, bytes_amount, timeout, datagrams_amount, wait_flag = False)
             sock.sendto(str.encode(temp), addr)
             continue
 
-    return data, addr, exc_flag
+    user['datagram_count_in'] = datagram_count_in
+
+    return data, user
 
 
-def echo(addr, body):
-    udp_send(body.encode('utf-8'), addr=addr, bytes_amount=UDP_BUFFER_SIZE, datagrams_amount=1)
+
+def udp_recv_from_new_user(sock, bytes_amount):
+    data = bytes()     
+    sock.settimeout(None)
+    data_temp, addr = sock.recvfrom(bytes_amount)
+
+    print('data_temp', data_temp)
+
+    # data_temp = data_temp.decode('utf-8')
+
+    seq_num = int(data_temp[:5])
+
+    if seq_num == 0:
+        datagram_count_in = seq_num + 1
+        data += bytes(data_temp[5:])
+
+        temp = format(datagram_count_in, '05d')
+        sock.settimeout(None)
+        sock.sendto(str(temp).encode('utf-8'), addr)
+
+        new_user = {
+            'address' : addr,
+            'datagram_count_in' : datagram_count_in,
+            'datagram_count_out' : 0,
+            'request' : data
+        }
+
+    return new_user
+
+
+
+
+# def echo(addr, body):
+    
 
 
 def echo_thread(user):
+    global users_echo_info
+    
+    dunamic_sock_num = random.randint(50050, 50100)
+    echo_sock = create_sock(dunamic_sock_num)
     ip_addr = user['address']
     print(f'[D][{datetime.datetime.now()}] Echo thread for user [{ip_addr}] was started!')
-    echo(user['address'], user['params'])
-    # download()
+    # echo(user['address'], user['params'])
+    request = user['request'].decode('utf-8')
+    command, params = request.split(' ')
+    print('For', str(format(dunamic_sock_num, '05d') + params).encode('utf-8'))
+    user['request'] = str(format(dunamic_sock_num, '05d') + params).encode('utf-8')
+    user = udp_send(echo_sock, user, user['request'], bytes_amount=UDP_BUFFER_SIZE, datagrams_amount=1)
+    echo_sock.close()
+
+    LOCK_ECHO.acquire(True)
+    # users_echo_info.remove()
+    LOCK_ECHO.release()
 
 
 def download_thread(user):
@@ -141,8 +192,12 @@ def download_thread(user):
     # download()
 
 
-def udp_send(sock, data, addr, bytes_amount, datagrams_amount):
-    global datagram_count_out
+def udp_send(sock, user, data, bytes_amount, datagrams_amount):
+    # global datagram_count_out
+    addr = user['address']
+    datagram_count_in = user['datagram_count_in']
+    datagram_count_out = user['datagram_count_out']
+
     fl = False
     datagram_count_out_begin = int(datagram_count_out)
     data_temp = bytes(data)
@@ -150,6 +205,7 @@ def udp_send(sock, data, addr, bytes_amount, datagrams_amount):
     seq_num = (-1, '127.0.0.1')
     while True:
         for i in range(i_temp, datagrams_amount):
+            print('datagram_count_out', datagram_count_out)
             temp = format(datagram_count_out, '05d').encode('utf-8')
             data_part = data[:bytes_amount]
             data_part = temp + data_part
@@ -177,13 +233,11 @@ def udp_send(sock, data, addr, bytes_amount, datagrams_amount):
                 pass
 
         if not fl:
-            # print('A0A0', datagram_count_out)
             sock.settimeout(8)
             seq_num = sock.recvfrom(UDP_BUFFER_SIZE + 5)
             if len(seq_num) > 5:
                 raise Exception
 
-            # print('A1A1', seq_num)
         fl = False
         sock.settimeout(None)
 
@@ -193,7 +247,9 @@ def udp_send(sock, data, addr, bytes_amount, datagrams_amount):
             dd = datagram_count_out_begin + datagrams_amount
 
         if datagram_count_out == int(seq_num[0]) and datagram_count_out == dd:
-            return True
+            user['datagram_count_in'] = datagram_count_in
+            user['datagram_count_in'] = datagram_count_out
+            return user
         else:
             datagram_count_out = int(seq_num[0])
 
@@ -209,97 +265,83 @@ def udp_send(sock, data, addr, bytes_amount, datagrams_amount):
 
 def download_service_thread(name):
     users_download_info = []
-    print('Download service thread started!')
+    print('[DS] Download service thread started!')
 
     server_download_sock = create_sock(DOWNLOAD_SERVICE_PORT)
 
     while 1:
         print(f'[DS][{datetime.datetime.now()}] Waiting for command')
-        request, address, a = udp_recv_1(server_download_sock, UDP_BUFFER_SIZE, None, 1, True)
-
-        new_user = {
-            'address' : address,
-            'datagram_count_in' : 0,
-            'datagram_count_out' : 0
-        }
+        new_user = udp_recv_from_new_user(server_download_sock, UDP_BUFFER_SIZE)
 
         users_download_info.append(new_user)
 
-        thread = threading.Thread(target=download_thread, args=new_user)
+        thread = threading.Thread(target=download_thread, args=(new_user,))
         thread.start()
     
-        request = request.decode('utf-8')
+        request = new_user['request'].decode('utf-8')
         print(f'[DS][{datetime.datetime.now()}] get a command: {request}')
 
-    # time.sleep(5)
-
-    print('End of download service thread!')
+    print('[DS] End of download service thread!')
 
 
 def upload_service_thread(name):
     users_upload_info = []
 
-    print('Upload service thread started!!')
+    print('[US] Upload service thread started!!')
 
     server_upload_sock = create_sock(UPLOAD_SERVICE_PORT)
 
     time.sleep(5)
 
-    print('End of upload!')
+    print('[US] End of upload service thread!')
 
 
 def echo_service_thread(name):
-    users_echo_info = []
+    global users_echo_info
 
-    print('Echo service thread started!!')
+    print('[ES] Echo service thread started!')
 
     server_echo_sock = create_sock(ECHO_SERVICE_PORT)
 
     while 1:
-        print(f'[DS][{datetime.datetime.now()}] Waiting for command')
-        request, address, a = udp_recv_1(server_echo_sock, UDP_BUFFER_SIZE, None, 1, True)
+        print(f'[ES][{datetime.datetime.now()}] Waiting for command')
+        new_user = udp_recv_from_new_user(server_echo_sock, UDP_BUFFER_SIZE)
 
-        request = request.decode('utf-8')
-        command, params = request.split(' ')
+        request = new_user['request'].decode('utf-8')
 
-        new_user = {
-            'address' : address,
-            'datagram_count_in' : 0,
-            'datagram_count_out' : 0,
-            'params' : params
-        }
-
+        LOCK_ECHO.acquire(True)
         users_echo_info.append(new_user)
+        LOCK_ECHO.release()
 
-        thread = threading.Thread(target=echo_thread, args=new_user)
+        thread = threading.Thread(target=echo_thread, args=(new_user,))
         thread.start()
           
-        print(f'[DS][{datetime.datetime.now()}] get a command: {request}')
+        print(f'[ES][{datetime.datetime.now()}] get a command: {request}')
 
-    # time.sleep(5)
-
-    print('End of echo service thread!')
+    print('[ES] End of echo service thread!')
 
 
 def time_service_thread(name):
     users_time_info = []
     
-    print('Time service thread started!!')
+    print('[TS] Time service thread started!')
 
     server_time_sock = create_sock(TIME_SERVICE_PORT)
 
     time.sleep(5)
 
-    print('End of time thread!')
+    print('[TS] End of time service thread!')
 
 
 
 ########################################### MAIN ###########################################
+print('================================================================')
 print('UDP Server!')
 print('Services: \n1. Download File (PORT 50001)\
                 \n2. Upload File (PORT 50002)\
                 \n3. Echo (PORT 50003)\
                 \n4. Get Time (PORT 50004)')
+                
 thread1 = threading.Thread(target=download_service_thread, args='1')
 thread1.start()
 thread2 = threading.Thread(target=upload_service_thread, args='2')
@@ -309,7 +351,9 @@ thread3.start()
 thread4 = threading.Thread(target=time_service_thread, args='4')
 thread4.start()
 
+print('================================================================')
 print('[MAIN Thread] Service threads were started! Print [exit] to exit')
+print('================================================================')
 
 # while(1):
 #     exit_input = input()
